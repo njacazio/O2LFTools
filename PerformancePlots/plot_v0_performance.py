d6c9926fdea1315c5f319b580d8ff3c1fccd00b0
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2022-11-14 21:26:27 +0100

    Update

--- PerformancePlots/plot_v0_performance.py
+++ PerformancePlots/plot_v0_performance.py
@@ -5,25 +5,44 @@ Script to plot the V0 performance plots
 """
 
 from ROOT import TFile, gPad, TH1, TLine, TF1
-from utils import draw_nice_canvas, draw_nice_label
+from utils import draw_nice_canvas, draw_nice_label, draw_nice_frame
 import argparse
 
 
-def main(fname, rebin=2,
+def main(fname, rebin=-1,
          tag="LHC22f",
-         particle="K0s"):
+         particle="K0s",
+         show_fit_range=False,
+         show_intermediate_steps=True,
+         bkg_function="pol2",
+         scale_y_range=1.1):
     f = TFile(fname, "READ")
     # f.ls()
-    xrange = [0.43, 0.54]
-    fitrange = [0.48, 0.52]
-    if particle in ["Lambda", "AntiLambda"]:
-        xrange = [1.06, 1.15]
-        fitrange = [1.1, 1.14]
-        dn = f"qa-k0s-tracking-efficiency/Lambda/h_mass"
-        if particle == "AntiLambda":
-            dn = f"qa-k0s-tracking-efficiency/AntiLambda/h_mass"
-    else:
-        dn = f"qa-k0s-tracking-efficiency/Test/h_mass"
+    xrange = {"K0s": [0.43, 0.54],
+              "Lambda": [1.086, 1.14],
+              "AntiLambda": [1.086, 1.14],
+              "XiPlus": [1.26, 1.36],
+              "XiMinus": [1.26, 1.36],
+              "OmegaMinus": [1.62, 1.71],
+              "OmegaPlus": [1.62, 1.71]}[particle]
+    fitrange = {"K0s": [0.48, 0.52],
+                "Lambda": [1.1, 1.14],
+                "AntiLambda": [1.1, 1.14],
+                "XiPlus": [1.31, 1.33],
+                "XiMinus": [1.31, 1.33],
+                "OmegaMinus": [1.66, 1.68],
+                "OmegaPlus": [1.66, 1.68]}[particle]
+    dn = {"K0s": "qa-k0s-tracking-efficiency/Test/h_mass",
+          "Lambda": "qa-k0s-tracking-efficiency/Lambda/h_mass",
+          "AntiLambda": "qa-k0s-tracking-efficiency/AntiLambda/h_mass",
+          "XiPlus": "cascade-qa/hMassXiPlus",
+          "XiMinus": "cascade-qa/hMassXiMinus",
+          "OmegaMinus": "cascade-qa/hMassOmegaMinus",
+          "OmegaPlus": "cascade-qa/hMassOmegaPlus"}[particle]
+    particle_mass = {"K0s": 0.497614,
+                     "Lambda": 1.115683, "AntiLambda": 1.115683,
+                     "XiPlus": 1.32171, "XiMinus": 1.32171,
+                     "OmegaPlus": 1.672, "OmegaMinus": 1.672}[particle]
     print(xrange)
     print(dn)
     f.Get(dn).ls()
@@ -33,24 +52,48 @@ def main(fname, rebin=2,
     if rebin > 0:
         h.Rebin(rebin)
     h.SetBit(TH1.kNoStats)
-    h.GetYaxis().SetTitle(
-        f"Counts per {h.GetXaxis().GetBinWidth(1)*1000.0:.1f} MeV/#it{{c}}^{{2}}")
+    h.GetYaxis().SetTitle(f"Normalized counts per {h.GetXaxis().GetBinWidth(1)*1000.0:.1f} MeV/#it{{c}}^{{2}}")
     h.SetBit(TH1.kNoTitle)
     h.SetMarkerStyle(20)
     h.SetMarkerSize(1.3)
     h.SetMarkerColor(1)
     h.SetLineColor(1)
+    h.GetXaxis().SetTitle("#it{m} (GeV/#it{c}^{2})")
     h.GetXaxis().SetRangeUser(*xrange)
-    can = draw_nice_canvas("V0s", logx=False, logz=True)
-    h.Draw("PE")
+    h.Scale(1.0/h.Integral())
+    h.SetMaximum(h.GetMaximum()*scale_y_range)
+    can = draw_nice_canvas("V0s"+particle, logx=False, logz=True)
+    draw_nice_frame(can, xrange, h, h, h)
+    h.Draw("PESAME")
+    # Functions
     fsig = TF1("fsig", "gaus", *xrange)
-    fbkg = TF1("fbkg", "pol2", *xrange)
+    fsig.SetParameter(1, particle_mass)
+    fsig.SetParLimits(1, particle_mass+0.1, particle_mass-0.1)
+    fsig_simple_bkg = TF1("fsig_simple_bkg", "gaus(0)+pol0(3)", *xrange)
+    fsig_simple_bkg.SetLineColor(3)
+    fsig_simple_bkg.SetLineStyle(2)
+    fbkg = TF1("fbkg", f"{bkg_function}", *xrange)
     fbkg.SetLineColor(1)
     fbkg.SetLineStyle(2)
-    fsum = TF1("fsum", "gaus(0)+pol2(3)", *xrange)
-    h.Fit(fsig, "QNR", "", *fitrange)
+    fsum = TF1("fsum", f"gaus(0)+{bkg_function}(3)", *xrange)
+    h.Fit(fsig, "N", "", *fitrange)
     for i in range(fsig.GetNpar()):
-        fsum.SetParameter(i, fsig.GetParameter(i))
+        fsig_simple_bkg.SetParName(i, fsig.GetParName(i))
+        fsig_simple_bkg.SetParameter(i, fsig.GetParameter(i))
+        fsig_simple_bkg.SetParError(i, fsig.GetParError(i))
+    h.Fit(fsig_simple_bkg, "N", "", *fitrange)
+    if show_intermediate_steps:
+        fsig.DrawClone("SAME")
+        fsig_simple_bkg.DrawClone("SAME")
+
+    for i in range(fsig.GetNpar()):
+        fsum.SetParName(i, fsig.GetParName(i))
+        fsum.SetParameter(i, fsig_simple_bkg.GetParameter(i))
+        if i > 0:
+            limits = [fsig_simple_bkg.GetParameter(i) - 10 * fsig_simple_bkg.GetParError(i),
+                      fsig_simple_bkg.GetParameter(i) + 10 * fsig_simple_bkg.GetParError(i)]
+            print(fsum.GetParName(i), limits)
+            fsum.SetParLimits(i, *limits)
     h.Fit(fsum, "QNR", "", *xrange)
     for i in range(fsig.GetNpar()):
         fsig.SetParameter(i, fsum.GetParameter(i))
@@ -58,26 +101,26 @@ def main(fname, rebin=2,
         fbkg.SetParameter(i, fsum.GetParameter(i+fsig.GetNpar()))
     fsum.Draw("same")
     fbkg.Draw("same")
-    k0mass = 0.497614
-    lambdamass = 1.115683
-    if particle in ["Lambda", "AntiLambda"]:
-        line = TLine(lambdamass, 0, lambdamass, h.GetMaximum())
-    else:
-        line = TLine(k0mass, 0, k0mass, h.GetMaximum())
-    line.SetLineStyle(2)
-    line.Draw()
+    lines = [TLine(particle_mass, h.GetMinimum(), particle_mass, h.GetMaximum())]
+    if show_fit_range:
+        lines.append(TLine(fitrange[0], h.GetMinimum(), fitrange[0], h.GetMaximum()))
+        lines.append(TLine(fitrange[1], h.GetMinimum(), fitrange[1], h.GetMaximum()))
+    for i in lines:
+        i.SetLineStyle(2)
+        i.Draw()
 
-    draw_nice_label("ALICE Performance", 0.2, 0.85, s=0.04)
-    if particle == "Lambda":
-        draw_nice_label("#Lambda", 0.92, 0.92, s=0.05, align=33)
-    elif particle == "AntiLambda":
-        draw_nice_label("#bar{#Lambda}", 0.92, 0.92, s=0.05, align=33)
-    else:
-        draw_nice_label("K^{0}_{s}", 0.92, 0.92, s=0.05, align=33)
+    label_y = 0.88
+    label_x = 0.18
+    # draw_nice_label("ALICE Performance", label_x, label_y, s=0.04)
+    particle_symbol = {"Lambda": "#Lambda", "AntiLambda": "#bar{#Lambda}",
+                       "K0s": "K^{0}_{S}",
+                       "XiPlus": "#Xi^{+}", "XiMinus": "#Xi^{-}",
+                       "OmegaPlus": "#bar{#Omega}^{+}", "OmegaMinus": "#Omega^{-}"}[particle]
+    draw_nice_label(particle_symbol, 0.92, 0.92, s=0.05, align=33)
     for i in enumerate([tag, "Gaussian fit:",
                         f"#mu = {fsum.GetParameter(1)*1000.0:.3f} #pm {fsum.GetParError(1)*1000.0:.3f} MeV/#it{{c}}^{{2}}",
                         f"#sigma = {fsum.GetParameter(2)*1000.0:.3f} #pm {fsum.GetParError(2)*1000.0:.3f} MeV/#it{{c}}^{{2}}"]):
-        draw_nice_label(i[1], 0.2, 0.8 - 0.04*i[0])
+        draw_nice_label(i[1], label_x, label_y - 0.04*(i[0]+1), s=0.03)
     gPad.Modified()
     gPad.Update()
     input("Press enter to continue")
@@ -88,8 +131,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("fname", help="Input file")
     parser.add_argument("--charge", default="Pos", help="Particle charge")
-    parser.add_argument("--particle", default="K0s", choices=["K0s", "Lambda", "AntiLambda"], help="Particle type")
+    parser.add_argument("--particle", default="K0s", choices=["K0s", "Lambda", "AntiLambda", "XiPlus", "XiMinus", "OmegaMinus", "OmegaPlus"],
+                        nargs="+", help="Particle type")
+    parser.add_argument("--bkg_function", default="pol2", help="Function to use as background")
+    parser.add_argument("--rebin", type=int, default=-1, help="Times to rebin")
+    parser.add_argument("--show_fit_range", action="store_true", help="Show fit range")
+    parser.add_argument("--show_intermediate_steps", action="store_true", help="Show intermediate steps")
     args = parser.parse_args()
 
-    main(args.fname, particle=args.particle)
-    # main(args.fname, args.charge, args.part)
+    if type(args.particle) == str:
+        args.particle = [args.particle]
+    for i in args.particle:
+        main(args.fname,
+             particle=i,
+             bkg_function=args.bkg_function,
+             rebin=args.rebin,
+             show_fit_range=args.show_fit_range,
+             show_intermediate_steps=args.show_intermediate_steps)
--- PerformancePlots/utils.py
+++ PerformancePlots/utils.py
@@ -18,3 +18,60 @@ def draw_nice_label(l, x=0.7, y=0.5, s=0.035, yd=0, align=11):
 
 
 nice_canvases = {}
+
+
+def draw_nice_canvas(name, x=800, y=800, logx=False, logy=False, logz=True, title=None, replace=True, extend_right=False):
+    global nice_canvases
+    if not replace and name in nice_canvases:
+        c = nice_canvases[name]
+        c.cd()
+        if title is not None:
+            c.SetTitle(title)
+        return c
+    if title is None:
+        title = name
+    right_margin = 0.05
+    if extend_right:
+        x = 1200
+        right_margin = 0.317195
+    c = TCanvas(name, title, x, y)
+    c.SetLogx(logx)
+    c.SetLogy(logy)
+    c.SetLogz(logz)
+    c.SetTicky()
+    c.SetTickx()
+    c.SetLeftMargin(0.15)
+    c.SetBottomMargin(0.15)
+    c.SetRightMargin(right_margin)
+    c.SetTopMargin(0.05)
+    c.Draw()
+    nice_canvases[name] = c
+    return c
+
+
+nice_frames = {}
+
+
+def draw_nice_frame(c, x, y, xt, yt):
+    c.cd()
+    global nice_frames
+    if type(x) is not list:
+        x = [x.GetXaxis().GetBinLowEdge(1),
+             x.GetXaxis().GetBinUpEdge(x.GetNbinsX())]
+    if type(y) is not list:
+        if "TH2" in y.ClassName():
+            y = [y.GetYaxis().GetBinLowEdge(1),
+                 y.GetYaxis().GetBinUpEdge(y.GetYaxis().GetNbins())]
+        else:
+            y = [y.GetMinimum(), y.GetMaximum()]
+    if not type(xt) is str:
+        xt = xt.GetXaxis().GetTitle()
+    if not type(yt) is str:
+        yt = yt.GetYaxis().GetTitle()
+    frame = c.DrawFrame(x[0], y[0], x[1], y[1], f";{xt};{yt}")
+    frame.GetYaxis().SetTitleSize(0.04)
+    frame.GetXaxis().SetTitleSize(0.04)
+    frame.GetXaxis().SetTitleOffset(1.25)
+    frame.SetDirectory(0)
+    nice_frames[c.GetName()] = frame
+    return frame
